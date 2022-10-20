7589079e50ee7ec43d8f5786b1a40993303cfbe6
arielmoguillansky <arielmogui92@gmail.com>
2022-10-20 09:21:34 -0300

    updated prod credentials

--- database/db.php
+++ database/db.php
@@ -1,15 +1,15 @@
 <?php
   // production db connection
-  // $host = 'sql10.freemysqlhosting.net';
-  // $user = 'sql10527870';
-  // $pwd = 'eGdDmcRndB';
-  // $dbName = 'sql10527870';
-  
+  $host = 'sql10.freemysqlhosting.net';
+  $user = 'sql10527870';
+  $pwd = 'eGdDmcRndB';
+  $dbName = 'sql10527870';
+
   // local db connectcion
-  $host = 'localhost';
-  $user = 'root';
-  $pwd = '';
-  $dbName = 'bh_news';
+  // $host = 'localhost';
+  // $user = 'root';
+  // $pwd = '';
+  // $dbName = 'bh_news';
   $connection = mysqli_connect($host, $user, $pwd, $dbName);
   if(!$connection) {
     die("DB connection failed");
